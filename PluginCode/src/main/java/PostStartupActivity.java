d250b1f0b058f48fd0efc089feabf07752ea2665
relhajj <46651402+relhajj@users.noreply.github.com>
2020-06-11 01:31:42 -0600

    Onload Editors 2

--- PluginCode/src/main/java/PostStartupActivity.java
+++ PluginCode/src/main/java/PostStartupActivity.java
@@ -1,58 +1,43 @@
-import com.intellij.openapi.Disposable;
-import com.intellij.openapi.extensions.ExtensionPointChangeListener;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.FileEditorManagerAdapter;
 import com.intellij.openapi.fileEditor.FileEditorManagerEvent;
 import com.intellij.openapi.fileEditor.FileEditorManagerListener;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
-import com.intellij.openapi.vfs.AsyncFileListener;
+import com.intellij.openapi.startup.StartupManager;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.vfs.VirtualFileManager;
-import com.intellij.openapi.vfs.VirtualFileManagerListener;
-import com.intellij.openapi.vfs.newvfs.BulkFileListener;
-import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
 import com.intellij.util.messages.MessageBus;
 import com.intellij.util.messages.MessageBusConnection;
-import org.jetbrains.annotations.Nullable;
 import smr.cs.ualberta.libcomp.DatabaseAccess;
 import org.jetbrains.annotations.NotNull;
 import smr.cs.ualberta.libcomp.action.ReplacementAction;
-
-import javax.management.remote.JMXConnector;
 import javax.swing.*;
 import java.io.IOException;
-import java.util.List;
 
 /**
  * PostStartupActivity class is triggered on load to start the plugin
  */
 
 public class PostStartupActivity implements StartupActivity {
-
-
     @Override
     public void runActivity(@NotNull Project project) {
-        DatabaseAccess dataAccessObject = new DatabaseAccess();
 
-        MessageBus messageBus = project.getMessageBus();
-        messageBus.connect().subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, new FileEditorManagerAdapter() {
+        DatabaseAccess dataAccessObject = new DatabaseAccess();
+        MessageBusConnection connection = project.getMessageBus().connect(project);
+        connection.subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, new FileEditorManagerListener() {
             @Override
             public void fileOpened(@NotNull FileEditorManager source, @NotNull VirtualFile file) {
-                super.fileOpened(source, file);
-
-
-
+               // super.fileOpened(source, file);
             }
 
             @Override
             public void fileClosed(@NotNull FileEditorManager source, @NotNull VirtualFile file) {
-                super.fileClosed(source, file);
+               // super.fileClosed(source, file);
             }
 
             @Override
             public void selectionChanged(@NotNull FileEditorManagerEvent event) {
-                super.selectionChanged(event);
+               // super.selectionChanged(event);
                 ReplacementAction actionPerformed = new ReplacementAction();
                 try {
                     actionPerformed.detectAllOpenEditors();
@@ -73,8 +58,6 @@ public class PostStartupActivity implements StartupActivity {
                     e.printStackTrace();
                 }
             });
-            
-            
         } catch (IOException e) {
             e.printStackTrace();
         }
